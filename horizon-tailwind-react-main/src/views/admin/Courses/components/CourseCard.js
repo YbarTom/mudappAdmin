2e26d19cdba3c51de14dbf11ac02f720c0bc39e3
YbarTom <a22tomybanog@inspedralbes.cat>
2024-03-14 15:36:51 +0100

    fino filipino trash icon

--- horizon-tailwind-react-main/src/views/admin/Courses/components/CourseCard.js
+++ horizon-tailwind-react-main/src/views/admin/Courses/components/CourseCard.js
@@ -1,18 +1,18 @@
 import PropTypes from "prop-types";
 import React from "react";
-import styled from "styled-components"; // Import styled-components
-import {ProgressBarText}from "./ProgressBarText"; // Import ProgressBarText component
-import BannerFoto from "assets/img/ColleBree/frame-85.png";
-// Define styled components
+import styled from "styled-components";
+import trash from "assets/img/ColleBree/trash.png"; // Importa el icono de la basura
+import BannerFoto from "assets/img/ColleBree/frame-85.png"; // Importa la imagen de fondo
+
 const CourseCardWrapper = styled.div`
-background-color: white;
-    border-radius: 16px;
-    box-shadow: #1b254014;
-    display: flex;
-    flex-direction: column;
-    overflow: hidden;
-    position: relative;
-    width: 335px;
+  background-color: white;
+  border-radius: 16px;
+  box-shadow: #1b254014;
+  display: flex;
+  flex-direction: column;
+  overflow: hidden;
+  position: relative;
+  width: 335px;
 `;
 
 const Frame = styled.div`
@@ -26,61 +26,61 @@ const Frame = styled.div`
 `;
 
 const FrameWrapper = styled.div`
-    align-items: flex-start;
-    align-self: stretch;
-    display: flex;
-    flex: 0 0 auto;
-    flex-direction: column;
-    gap: 20px;
-    padding: 20px 15px;
-    position: relative;
-    width: 100%;
+  align-items: flex-start;
+  align-self: stretch;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  padding: 20px 15px;
+  position: relative;
+  width: 100%;
 `;
 
 const InnerDiv = styled.div`
-    align-items: flex-start;
-    align-self: stretch;
-    display: flex;
-    flex: 0 0 auto;
-    flex-direction: column;
-    gap: 10px;
-    position: relative;
-    width: 100%;
+  align-self: stretch;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
 `;
 
 const Text = styled.div`
-    font-family: "Manrope", Helvetica;
-    font-size: 18px;
-    font-weight: 700;
-    letter-spacing: -0.63px;
-    line-height: 21.6px;
-    margin-top: -1px;
-    position: relative;
-    width: 302px;
-    left: 0;
+  font-family: "Manrope", Helvetica;
+  font-size: 18px;
+  font-weight: 700;
+  letter-spacing: -0.63px;
+  line-height: 21.6px;
 `;
 
+const IconWrapper = styled.div`
+  align-self: flex-end;
+  margin-top: auto;
+`;
 
+const Icon = styled.img`
+  width: 20px;
+  height: 20px;
+`;
 
-const CourseCard = ({ text, onClick, foto=BannerFoto}) => {
-    return (
-        <CourseCardWrapper onClick={onClick}>
-            <Frame foto={foto} />
-            <FrameWrapper>
-                <InnerDiv>
-                    <Text>{text}</Text>
-                    
-                </InnerDiv>
-            </FrameWrapper>
-        </CourseCardWrapper>
-    );
+const CourseCard = ({ text, onClick, foto = BannerFoto }) => {
+  return (
+    <CourseCardWrapper onClick={onClick}>
+      <Frame foto={foto} />
+      <FrameWrapper>
+        <InnerDiv>
+          <Text>{text}</Text>
+          <IconWrapper>
+            <Icon src={trash} alt="Icono de Basura" />
+            
+          </IconWrapper>
+        </InnerDiv>
+      </FrameWrapper>
+    </CourseCardWrapper>
+  );
 };
 
-// Define prop types
 CourseCard.propTypes = {
-    text: PropTypes.string,
-    progress: PropTypes.number,
-    onClick: PropTypes.func,
+  text: PropTypes.string,
+  onClick: PropTypes.func,
 };
 
 export default CourseCard;
