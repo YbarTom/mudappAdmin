00ecb16d8642c6f8aaab2b372f9e6e769287bbd7
MikiDix <a22miqdervil@inspedralbes.cat>
2024-03-15 15:34:25 +0100

    xd

--- horizon-tailwind-react-main/src/views/admin/Courses/index.jsx
+++ horizon-tailwind-react-main/src/views/admin/Courses/index.jsx
@@ -6,81 +6,12 @@ import routes from 'routes';
 import { Link } from "react-router-dom";
 
 const Test = () => {
-  const [formData, setFormData] = useState({
-    title: '',
-    photo: '',
-    levels: []
-  });
-  const [showSecondForm, setShowSecondForm] = useState(false);
-
-  const handleChangeCourse = (e) => {
-    setFormData({ ...formData, [e.target.name]: e.target.value });
-  };
-
-  const handleChangeCourseLevels = (e) => {
-    setFormData({ ...formData, [e.target.name]: e.target.value });
-  };
-
-  const handleChange = (e) => {
-    setFormData({ ...formData, [e.target.name]: e.target.value });
-  };
-
-  const handleSubmitCourse = async (e) => {
-    e.preventDefault();
-
-    try {
-      const response = await fetch('http://localhost:3001/guardar-datos-course', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-          title: formData.title,
-          photo: formData.photo,
-          levels: formData.levels // Aquí estamos enviando directamente el array de niveles
-        })
-      });
-
-      if (response.ok) {
-        console.log('Datos guardados exitosamente.');
-        setShowSecondForm(true); // Mostrar el segundo formulario después de enviar el primero
-      } else {
-        console.error('Error al guardar datos.');
-      }
-    } catch (error) {
-      console.error('Error al enviar los datos:', error);
-    }
-  };
-
-  const handleSubmitLevel = async (e) => {
-    e.preventDefault();
-
-    try {
-      const response = await fetch('http://localhost:3001/guardar-datos-level', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(formData)
-      });
-
-      if (response.ok) {
-        console.log('Datos guardados exitosamente.');
-        setShowSecondForm(true); // Mostrar el segundo formulario después de enviar el primero
-      } else {
-        console.error('Error al guardar datos.');
-      }
-    } catch (error) {
-      console.error('Error al enviar los datos:', error);
-    }
-  };
-  
 
   return (
     <div>
       <div className="mt-3 w-[335px]">
-      <Link key={3} to={"/admin/NewCourse"}>
-        <ButtonLogOut text={"Create new course"} ></ButtonLogOut>
+        <Link key={3} to={"/admin/NewCourse"}>
+          <ButtonLogOut text={"Create new course"} ></ButtonLogOut>
         </Link>
       </div>
       {/* Charts */}
@@ -95,47 +26,14 @@ const Test = () => {
         <CourseCard text="Course Card 1" progress={90} />
 
         <Link key={3} to={"/admin/NewCourse"}>
-        <CourseCardPlus />
+          <CourseCardPlus />
         </Link>
       </div>
 
       {/* Tables & Charts */}
       <div className="mt-5 grid grid-cols-1 gap-5 xl:grid-cols-2">
         {/* Check Table */}
-        <div>
-          {!showSecondForm && ( // Mostrar el primer formulario si showSecondForm es falso
-            <div>
-              <h2>Introduce tus datos:</h2>
-              <form onSubmit={handleSubmitCourse}>
-                <div>
-                  <label htmlFor="title">Nombre del curso:</label>
-                  <input type="text" id="title" name="title" value={formData.title} onChange={handleChangeCourse} />
-                </div>
-                <div>
-                  <label htmlFor="photo">Foto:</label>
-                  <input type="text" id="photo" name="photo" value={formData.photo} onChange={handleChangeCourse} />
-                </div>
-                <div>
-                  <label htmlFor="levels">Número de niveles:</label>
-                  <input type="text" id="levels" name="levels" value={formData.levels} onChange={handleChangeCourseLevels} />
-                </div>
-                <button type="submit">Enviar</button>
-              </form>
-            </div>
-          )}
-          {showSecondForm && ( // Mostrar el segundo formulario si showSecondForm es verdadero
-            <div>
-              <h2>Segundo formulario:</h2>
-              <form onSubmit={handleSubmitLevel}>
-                <div>
-                  <label htmlFor="titleLevel">Nombre del curso2:</label>
-                  <input type="text" id="titleLevel" name="titleLevel" value={formData.titleLevel} onChange={handleChange} />
-                </div>
-                <button type="submit">Enviar</button>
-              </form>
-            </div>
-          )}
-        </div>
+
       </div>
     </div>
   );
--- horizon-tailwind-react-main/src/views/admin/NewCourse/index.jsx
+++ horizon-tailwind-react-main/src/views/admin/NewCourse/index.jsx
@@ -5,9 +5,19 @@ import CourseCardPlus from "./components/CourseCardPlus";
 
 const Test = () => {
   const [formData, setFormData] = useState({
-    titleCourse: '',
+    title: '',
+    photo: '',
+    levels: []
   });
   const [showSecondForm, setShowSecondForm] = useState(false);
+
+  const handleChangeCourse = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleChangeCourseLevels = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
 
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
@@ -22,7 +32,11 @@ const Test = () => {
         headers: {
           'Content-Type': 'application/json'
         },
-        body: JSON.stringify(formData)
+        body: JSON.stringify({
+          title: formData.title,
+          photo: formData.photo,
+          levels: formData.levels // Aquí estamos enviando directamente el array de niveles
+        })
       });
 
       if (response.ok) {
@@ -36,7 +50,7 @@ const Test = () => {
     }
   };
 
-  const handleSubmitLevel= async (e) => {
+  const handleSubmitLevel = async (e) => {
     e.preventDefault();
 
     try {
@@ -58,48 +72,40 @@ const Test = () => {
       console.error('Error al enviar los datos:', error);
     }
   };
-
   return (
     <div>
-      <div className="mt-3 w-[335px]">
-        <ButtonLogOut text={"Create new course"} ></ButtonLogOut>
-      </div>
-      {/* Charts */}
-      <div className="mt-5 grid grid-cols-1 gap-5 md:grid-cols-3">
-        <CourseCard text="Course Card 1" progress={90} />
-        
-      </div>
-
-      {/* Tables & Charts */}
-      <div className="mt-5 grid grid-cols-1 gap-5 xl:grid-cols-2">
-        {/* Check Table */}
+      {!showSecondForm && ( // Mostrar el primer formulario si showSecondForm es falso
         <div>
-          {!showSecondForm && ( // Mostrar el primer formulario si showSecondForm es falso
+          <h2>Introduce tus datos:</h2>
+          <form onSubmit={handleSubmitCourse}>
             <div>
-              <h2>Introduce tus datos:</h2>
-              <form onSubmit={handleSubmitCourse}>
-                <div>
-                  <label htmlFor="titleCourse">Nombre del curso:</label>
-                  <input type="text" id="titleCourse" name="titleCourse" value={formData.titleCourse} onChange={handleChange} />
-                </div>
-                <button type="submit">Enviar</button>
-              </form>
+              <label htmlFor="title">Nombre del curso:</label>
+              <input type="text" id="title" name="title" value={formData.title} onChange={handleChangeCourse} />
             </div>
-          )}
-          {showSecondForm && ( // Mostrar el segundo formulario si showSecondForm es verdadero
             <div>
-              <h2>Segundo formulario:</h2>
-              <form onSubmit={handleSubmitLevel}>
-                <div>
-                  <label htmlFor="titleLevel">Nombre del curso2:</label>
-                  <input type="text" id="titleLevel" name="titleLevel" value={formData.titleLevel} onChange={handleChange} />
-                </div>
-                <button type="submit">Enviar</button>
-              </form>
+              <label htmlFor="photo">Foto:</label>
+              <input type="text" id="photo" name="photo" value={formData.photo} onChange={handleChangeCourse} />
             </div>
-          )}
+            <div>
+              <label htmlFor="levels">Número de niveles:</label>
+              <input type="text" id="levels" name="levels" value={formData.levels} onChange={handleChangeCourseLevels} />
+            </div>
+            <button type="submit">Enviar</button>
+          </form>
         </div>
-      </div>
+      )}
+      {showSecondForm && ( // Mostrar el segundo formulario si showSecondForm es verdadero
+        <div>
+          <h2>Segundo formulario:</h2>
+          <form onSubmit={handleSubmitLevel}>
+            <div>
+              <label htmlFor="titleLevel">Nombre del curso2:</label>
+              <input type="text" id="titleLevel" name="titleLevel" value={formData.titleLevel} onChange={handleChange} />
+            </div>
+            <button type="submit">Enviar</button>
+          </form>
+        </div>
+      )}
     </div>
   );
 };
