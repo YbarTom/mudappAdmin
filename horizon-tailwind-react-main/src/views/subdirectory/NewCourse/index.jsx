891eb4b8efa0c1e5b231fa37ecd2f7e1dd073e3a
MikiDix <a22miqdervil@inspedralbes.cat>
2024-03-19 16:00:12 +0100

    levels dinamicos

--- horizon-tailwind-react-main/src/views/subdirectory/NewCourse/index.jsx
+++ horizon-tailwind-react-main/src/views/subdirectory/NewCourse/index.jsx
@@ -1,13 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import CourseSection from './components/CourseSection';
-import Lesson from './components/Lesson';
+import Level from './components/Level';
 import CourseSectionPlus from './components/CoursSectionPlus';
 
-import Level from './components/Level';
-
 const Test = () => {
-
-  var levels = []
+  const [levels, setLevels] = useState([]);
 
   const [formDataCourse, setFormDataCourse] = useState({
     title: '',
@@ -22,7 +19,7 @@ const Test = () => {
     title: '',
     idLevel: 0
   });
-  
+
   const [formDataFlashCard, setFormDataFlashCard] = useState({
     title: '',
   });
@@ -63,12 +60,10 @@ const Test = () => {
       const data = await response.json();
       console.log('ID del nuevo curso:', data.id);
 
-      // Actualizar el estado formDataLevel con el ID del curso recién guardado
       setFormDataLevel({ ...formDataLevel, idCourse: data.id });
 
       console.log('Datos guardados exitosamente.');
       setShowSecondForm(true);
-      // Aquí puedes hacer lo que quieras con el ID, por ejemplo, mostrarlo en la interfaz de usuario
     } catch (error) {
       console.error('Error al enviar los datos:', error);
     }
@@ -93,21 +88,18 @@ const Test = () => {
       var level = {
         title: formDataLevel.title,
         part: formDataLevel.part
-      }
+      };
 
-      levels.push(level)
-
-      console.log(level)
+      setLevels([...levels, level]);
 
       const data = await response.json();
       console.log('ID del nuevo level:', data.id);
 
-      // Actualizar el estado formDataLevel con el ID del curso recién guardado
       setFormDataLesson({ ...formDataLesson, idLevel: data.id });
 
       if (response.ok) {
         console.log('Datos guardados exitosamente level');
-        setShowThirdForm(true); // Mostrar el tercer formulario después de enviar el segundo
+        setShowThirdForm(true);
       } else {
         console.error('Error al guardar datos.');
       }
@@ -133,7 +125,7 @@ const Test = () => {
 
       if (response.ok) {
         console.log('Datos guardados exitosamente lesson');
-        setShowFourForm(true); // Mostrar el tercer formulario después de enviar el segundo
+        setShowFourForm(true);
       } else {
         console.error('Error al guardar datos.');
       }
@@ -141,82 +133,77 @@ const Test = () => {
       console.error('Error al enviar los datos:', error);
     }
   }
-  const handleSubmitFlashCard= async (e) => {
+  const handleSubmitFlashCard = async (e) => {
     setShowFourForm(true);
   }
 
   return (
-    <div style={{ display: 'grid', gridTemplateColumns: '1fr 1fr 1fr', marginTop:"10px"}}>
+    <div style={{ display: 'grid', gridTemplateColumns: '1fr 1fr 1fr', marginTop: "10px" }}>
       <div style={{ overflowY: 'auto', maxHeight: 'calc(100vh - 100px)' }}>
-        <Level/>
-        <Level/>
-        <Level/>
-        <Level/>
-        <Level/>
-        <Level/>
+        {levels.map((level, index) => (
+          <Level key={index} text={level.title} />
+        ))}
         <CourseSectionPlus></CourseSectionPlus>
       </div>
       <div> </div>
       <div>
-      {!showSecondForm && (
-        <div>
-          <h2>Introduce tus datos:</h2>
-          <form onSubmit={handleSubmitCourse}>
-            <div>
-              <label htmlFor="title">Nombre del curso:</label>
-              <input type="text" id="title" name="title" value={formDataCourse.title} onChange={handleChangeCourse} />
-            </div>
-            <div>
-              <label htmlFor="photo">Foto:</label>
-              <input type="text" id="photo" name="photo" value={formDataCourse.photo} onChange={handleChangeCourse} />
-            </div>
-            <button type="submit">Enviar</button>
-          </form>
-        </div>
-      )}
-      {showSecondForm && !showThirdForm && !showFourForm && ( // Mostrar el segundo formulario si showSecondForm es verdadero y el tercer formulario no está mostrándose
-        <div>
-          <h2>Segundo formulario:</h2>
-          <form onSubmit={handleSubmitLevel}>
-            <div>
-              <label htmlFor="title">Nombre del Nivel:</label>
-              <input type="text" id="title" name="title" value={formDataLevel.title} onChange={handleChangeLevel} />
-            </div>
-            <div>
-              <label htmlFor="part">Parte:</label>
-              <input type="number" id="part" name="part" value={formDataLevel.part} onChange={handleChangeLevel} />
-            </div>
-            <button type="submit">Enviar</button>
-          </form>
-        </div>
-      )}
-      {showThirdForm && !showFourForm && ( // Mostrar el tercer formulario si showThirdForm es verdadero
-        <div>
-          <h2>Tercer formulario:</h2>
-          <form onSubmit={handleSubmitLesson}>
-            <div>
-              <label htmlFor="title">Nombre de la lesson:</label>
-              <input type="text" id="title" name="title" value={formDataLesson.title} onChange={handleChangeLesson} />
-            </div>
-            <button type="submit">Enviar</button>
-          </form>
-          {/* Aquí colocarías el contenido y el formulario del tercer paso */}
-        </div>
-      )}
+        {!showSecondForm && (
+          <div>
+            <h2>Introduce tus datos:</h2>
+            <form onSubmit={handleSubmitCourse}>
+              <div>
+                <label htmlFor="title">Nombre del curso:</label>
+                <input type="text" id="title" name="title" value={formDataCourse.title} onChange={handleChangeCourse} />
+              </div>
+              <div>
+                <label htmlFor="photo">Foto:</label>
+                <input type="text" id="photo" name="photo" value={formDataCourse.photo} onChange={handleChangeCourse} />
+              </div>
+              <button type="submit">Enviar</button>
+            </form>
+          </div>
+        )}
+        {showSecondForm && !showThirdForm && !showFourForm && (
+          <div>
+            <h2>Segundo formulario:</h2>
+            <form onSubmit={handleSubmitLevel}>
+              <div>
+                <label htmlFor="title">Nombre del Nivel:</label>
+                <input type="text" id="title" name="title" value={formDataLevel.title} onChange={handleChangeLevel} />
+              </div>
+              <div>
+                <label htmlFor="part">Parte:</label>
+                <input type="number" id="part" name="part" value={formDataLevel.part} onChange={handleChangeLevel} />
+              </div>
+              <button type="submit">Enviar</button>
+            </form>
+          </div>
+        )}
+        {showThirdForm && !showFourForm && (
+          <div>
+            <h2>Tercer formulario:</h2>
+            <form onSubmit={handleSubmitLesson}>
+              <div>
+                <label htmlFor="title">Nombre de la lesson:</label>
+                <input type="text" id="title" name="title" value={formDataLesson.title} onChange={handleChangeLesson} />
+              </div>
+              <button type="submit">Enviar</button>
+            </form>
+          </div>
+        )}
 
-      {showFourForm && ( // Mostrar el tercer formulario si showThirdForm es verdadero
-        <div>
-          <h2>Cuarto formulario:</h2>
-          <form onSubmit={handleSubmitFlashCard}>
-            <div>
-              <label htmlFor="title">Nombre de la lesson:</label>
-              <input type="text" id="title" name="title" value={formDataFlashCard.title} onChange={handleChangeFlashCard} />
-            </div>
-            <button type="submit">Enviar</button>
-          </form>
-          {/* Aquí colocarías el contenido y el formulario del tercer paso */}
-        </div>
-      )}
+        {showFourForm && (
+          <div>
+            <h2>Cuarto formulario:</h2>
+            <form onSubmit={handleSubmitFlashCard}>
+              <div>
+                <label htmlFor="title">Nombre de la lesson:</label>
+                <input type="text" id="title" name="title" value={formDataFlashCard.title} onChange={handleChangeFlashCard} />
+              </div>
+              <button type="submit">Enviar</button>
+            </form>
+          </div>
+        )}
       </div>
     </div>
   );
