e776995a229698c426c90fa72a2bfcf84c33748a
YbarTom <a22tomybanog@inspedralbes.cat>
2024-03-19 16:29:44 +0100

    flash card bar disseño fino

--- horizon-tailwind-react-main/src/views/subdirectory/NewCourse/components/FlashCardBar.js
+++ horizon-tailwind-react-main/src/views/subdirectory/NewCourse/components/FlashCardBar.js
@@ -1,19 +1,20 @@
 import React, { useState } from "react";
 
-
 const FlashCardBar = () => {
-    
-  return (
-    <div style={{height:"86px", width:"100%",borderTopLeftRadius: "16px",borderTopRightRadius: "16px",display: 'grid', gridTemplateColumns: '1fr 1fr 1fr 1fr' }}>
-     <div style={{backgroundColor:"red",borderTopLeftRadius: "16px" }}></div>
-     <div style={{backgroundColor:"yellow"}}></div>
-     <div style={{backgroundColor:"green"}}></div>
-     <div style={{backgroundColor:"blue",borderTopRightRadius: "16px"}}></div>
+  const [selected, setSelected] = useState(0);
 
+  const handleSelect = (index) => {
+    setSelected(index);
+  };
+
+  return (
+    <div style={{ height: "86px", width: "100%", borderTopLeftRadius: "16px", borderTopRightRadius: "16px", display: 'grid', gridTemplateColumns: '1fr 1fr 1fr 1fr' }}>
+      <div style={{ borderTopLeftRadius: "16px", borderRight: "1px solid black", borderBottom: selected === 0 ? "none" : "1px solid black", display: "flex", alignItems: "center", justifyContent: "center" }} onClick={() => handleSelect(0)}>Complete</div>
+      <div style={{ borderRight: "1px solid black", borderBottom: selected === 1 ? "none" : "1px solid black", display: "flex", alignItems: "center", justifyContent: "center" }} onClick={() => handleSelect(1)}>Multiple Choice</div>
+      <div style={{ borderRight: "1px solid black", borderBottom: selected === 2 ? "none" : "1px solid black", display: "flex", alignItems: "center", justifyContent: "center" }} onClick={() => handleSelect(2)}>True False</div>
+      <div style={{ borderTopRightRadius: "16px", borderBottom: selected === 3 ? "none" : "1px solid black", display: "flex", alignItems: "center", justifyContent: "center" }} onClick={() => handleSelect(3)}>Relate</div>
     </div>
   );
 };
-
-
 
 export default FlashCardBar;
