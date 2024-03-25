a7e289fd7e355518c6df32fa8143bf98baa6bdf4
YbarTom <a22tomybanog@inspedralbes.cat>
2024-03-25 11:45:43 +0100

    a

--- horizon-tailwind-react-main/src/views/subdirectory/NewCourse/components/CreateLevel.js
+++ horizon-tailwind-react-main/src/views/subdirectory/NewCourse/components/CreateLevel.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { Input } from "./Input";
 import { ButtonLogOut } from "./randomButton";
 
-const WhiteBox = () => {
+const CreateLevel = () => {
     return (
         <div style={{
             backgroundColor: "white",
@@ -15,11 +15,13 @@ const WhiteBox = () => {
             alignItems: "center",
         }}>
             <div style={{ display: "flex", flexDirection: "column", gap: "20px" }}>
-               
+                <Input placeholder="Title" />
+                <Input placeholder="Foto" />
+                <ButtonLogOut text={"Create new course"} type={"blue"}></ButtonLogOut>
             </div>
 
         </div>
     );
 };
 
-export default WhiteBox;
+export default CreateLevel;
