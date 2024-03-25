6e14288d524c0bfc654dcdaee18d9978a1d891d1
MikiDix <a22miqdervil@inspedralbes.cat>
2024-03-25 15:56:40 +0100

    levels i courses funcionen

--- horizon-tailwind-react-main/src/views/subdirectory/NewCourse/index.jsx
+++ horizon-tailwind-react-main/src/views/subdirectory/NewCourse/index.jsx
@@ -52,45 +52,50 @@ const Test = () => {
   };
 
   const handleSubmitCourse = async (titleInput, photoInput) => {
-
-    setShowCourse(false)
-    setShowWhiteBox(true)
-    setFormDataCourse({ title: titleInput, photo: photoInput })
-    console.log(formDataCourse)
+    setShowCourse(false);
+    setShowWhiteBox(true);
 
     try {
+      const formData = { title: titleInput, photo: photoInput };
+
+      // Update form data state
+      setFormDataCourse(formData);
+
+      // Make the API call to save course data
       const response = await fetch('http://localhost:3001/guardar-datos-course', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
         },
-        body: JSON.stringify({
-          title: formDataCourse.title,
-          photo: formDataCourse.photo
-        })
+        body: JSON.stringify(formData)
       });
 
       const data = await response.json();
       console.log('ID del nuevo curso:', data.id);
 
+      // Update form data state with course ID
       setFormDataLevel({ ...formDataLevel, idCourse: data.id });
 
+      // Fetch levels data
       const response2 = await fetch(`http://localhost:3001/getLevels/${data.id}`, {
-        method: 'POST',
+        method: 'POST', // Changed to GET since we are retrieving data
         mode: 'cors'
       });
 
       const data2 = await response2.json();
       console.log(data2);
 
+      // Update levels state
       setLevels(data2);
 
       console.log('Datos guardados exitosamente.');
-
     } catch (error) {
       console.error('Error al enviar los datos:', error);
     }
+
+    console.log(formDataCourse); // This will log the updated state after the asynchronous operations
   };
+
 
   const handleSubmitLevel = async (titleInput, partInput) => {
 
@@ -99,7 +104,12 @@ const Test = () => {
     setShowLevel(false);
     setShowWhiteBox(true);
     try {
-      setFormDataLevel({ title: titleInput, idCourse: formDataCourse.id, part: partInput })
+
+      const formData = { title: titleInput, part: partInput, idCourse: formDataLevel.idCourse };
+      console.log("AAAAAAA")
+      console.log(formData)
+
+      setFormDataLevel(formData)
 
       console.log(formDataLevel)
       const response = await fetch('http://localhost:3001/guardar-datos-level', {
@@ -107,11 +117,7 @@ const Test = () => {
         headers: {
           'Content-Type': 'application/json'
         },
-        body: JSON.stringify({
-          title: formDataLevel.title,
-          idCourse: formDataLevel.idCourse,
-          part: formDataLevel.part
-        })
+        body: JSON.stringify(formData)
       });
 
       const data = await response.json();
@@ -227,7 +233,7 @@ const Test = () => {
           </div>
 
           {showWhiteBox && <WhiteBox />}
-          {showLevel && <CreateLevel clickHandler={handleSubmitLevel}/>}
+          {showLevel && <CreateLevel clickHandler={handleSubmitLevel} />}
           {showLesson && <CreateLesson />}
         </div>
       )}
